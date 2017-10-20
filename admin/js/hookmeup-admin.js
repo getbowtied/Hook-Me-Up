c5915856ec9152ec9930a6e40957f9a5a3b4db7a
Vanesa <vanesa@getbowtied.com>
2017-10-20 16:53:10 +0300

    ..

--- admin/js/hookmeup-admin.js
+++ admin/js/hookmeup-admin.js
@@ -1,17 +1,6 @@
 (function( $ ) {
 	'use strict';
 
-	setInterval(function(){
-		if( jQuery('#customize-preview').children('iframe').length > 1 ) {
-			jQuery( '.customize-control-kirki-editor .toggle-editor' ).addClass('btn-disabled');
-			jQuery( '.customize-control-kirki-editor .customize-control-content' ).addClass('disabled');
-		} else {
-		    jQuery( '.customize-control-kirki-editor .toggle-editor' ).removeClass('btn-disabled');
-		    jQuery( '.customize-control-kirki-editor .customize-control-content' ).removeClass('disabled');
-		}
-
-	}, 700);
-
 	$(document).on('click', '.toggle-editor', function(){
 		var buttons = document.getElementsByClassName('toggle-editor');
 
@@ -24,6 +13,21 @@
 				(buttons.item(i)).className = 'button button-primary toggle-editor button-close';
 			}
 		}
+
+		if( jQuery( '#customize-preview' ).hasClass( 'is-kirki-editor-open' ) ) {
+
+			setInterval(function(){
+				if( jQuery('#customize-preview').children('iframe').length > 1 ) {
+					jQuery( '.customize-control-kirki-editor .toggle-editor' ).addClass('btn-disabled');
+					jQuery( '.customize-control-kirki-editor .customize-control-content' ).addClass('disabled');
+				} else {
+				    jQuery( '.customize-control-kirki-editor .toggle-editor' ).removeClass('btn-disabled');
+				    jQuery( '.customize-control-kirki-editor .customize-control-content' ).removeClass('disabled');
+				}
+
+			}, 700);
+		}
+		
 	});
 
 	$(document).on('click', '.customize-section-back', function(){
